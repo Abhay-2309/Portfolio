cbe74dbab0847e2ae304dfe8fd0c7940b8ff4fdd
Abhay Sharma <abhayrsharma1000@gmail.com>
2024-08-31 22:50:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".typed",{
-    strings : ["Fronted Developer", "Sports Man", "Coding enthusiast"],
+    strings : ["Frontend Developer", "Sports Man", "Coding enthusiast"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
