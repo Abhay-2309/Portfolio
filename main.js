1e7f32bb998a0145eb8834f2a1828cf6c5088fe5
Abhay Sharma <abhayrsharma1000@gmail.com>
2024-08-31 22:24:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".typed",{
+    strings : ["Fronted Developer", "Sports Man", "Coding enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
